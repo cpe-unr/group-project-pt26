17ce4f617832188677b91f664b478bff7fab094b
Drake Lemon <dlemon@nevada.unr.edu>
2021-04-29 15:36:39 -0700

    echo class

--- Echo.cpp
+++ Echo.cpp
@@ -1,4 +1,27 @@
-/*
-*/
-
+/** @file */
 #include "Echo.h"
+#include <cmath>
+
+Echo::Echo(int delay) : delay(delay) {}
+
+unsigned char* Echo::processBuffer(unsigned char* buffer, int size){
+	unsigned char* newBuffer = new unsigned char[size];
+	for (int i = 0; i < delay; i++) {
+		newBuffer[i] = buffer[i];
+	}
+	for (int i = 0; i < size - delay; i++) {
+		newBuffer[i + delay] = (unsigned char)(round((float)(buffer[i + delay] - 127.5) * scaleFactor + (float)(newBuffer[i] - 127.5) * echoLevel  + 127.5));
+	}
+	return newBuffer;
+}
+
+short* Echo::processBuffer(short* buffer, int size){
+	short* newBuffer = new short[size / 2];
+	for (int i = 0; i < delay/2; i++) {
+		newBuffer[i] = buffer[i];
+	}
+	for (int i = 0; i < (size - delay)/2; i++) {
+		newBuffer[i + (delay / 2)] = buffer[i + (delay / 2)] * scaleFactor + newBuffer[i] * echoLevel;
+	}
+	return newBuffer;
+}
--- Echo.h
+++ Echo.h
@@ -1,11 +1,51 @@
-/*
-*/
-
+/** @file */
 #ifndef GROUP_PROJECT_ECHO_H
 #define GROUP_PROJECT_ECHO_H
 
+#include "Iprocessable.h"
+
+/**
+ * A const float giving the level of echos
+ */
+const float echoLevel = 0.5f;
+
+/**
+ * A const float giving the factor to scale audio by
+ */
+const float scaleFactor = 0.75f;
+
+/**
+ * A class for applying an echo
+ */
 class Echo : public Iprocessable{
 
+	/**
+ 	 * An int storing the delay in bytes for the echo
+ 	 */
+	int delay;
+public:
+
+	/**
+	 * A parameterized constructor setting delay
+	 * @param delay - The value to set delay to 
+	 */
+	Echo(int delay);
+
+	/**
+	 * Method that can be inherited to Echo, Normalization, and NoiseGate. Allows each class to process the given Wav File. Overload for 8 bit files. 
+	 * @param buffer - an array of unsigned chars constituting the audio data
+	 * @param size - an integer size of the buffer
+	 * @return - a processed array of unsigned chars
+	 */
+	unsigned char* processBuffer(unsigned char* buffer, int size) override;
+
+	/**
+	 * Method that can be inherited to Echo, Normalization, and NoiseGate. Allows each class to process the given Wav File. Overload for 8 bit files. 
+	 * @param buffer - an array of unsigned chars constituting the audio data
+	 * @param size - an integer size of the buffer
+	 * @return - a processed array of unsigned chars
+	 */
+	short* processBuffer(short* buffer, int size) override;
 };
 
 #endif //GROUP_PROJECT_ECHO_H
