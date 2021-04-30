43ca4ee32d487bc27b01960df21c973be488b1b2
Drake Lemon <dlemon@nevada.unr.edu>
2021-04-29 21:33:11 -0700

    Normalization class

--- Normal.cpp
+++ Normal.cpp
@@ -1,4 +1,33 @@
-/*
-*/
-
+/** @file */
 #include "Normal.h"
+#include <cmath>
+
+unsigned char* Normal::processBuffer(unsigned char* buffer, int size){
+	float maxVal = 0;
+	unsigned char* newBuffer = new unsigned char[size];
+	for (int i = 0; i < size; i++) {
+		if (abs((float)(buffer[i]) - 127.5) > maxVal) {
+			maxVal = abs((float)(buffer[i]) - 127.5);
+		}
+	}
+	float multiplier = 127.5 / maxVal;
+	for (int i = 0; i < size; i++) {
+		newBuffer[i] = (unsigned char)(floor(((float)(buffer[i]) - 127.5) * multiplier) + 127.5);
+	}
+	return newBuffer;
+}
+
+short* Normal::processBuffer(short* buffer, int size){
+	float maxVal = 0;
+	short* newBuffer = new short[size / 2];
+	for (int i = 0; i < size/2; i++) {
+		if (abs((float)(buffer[i])) > maxVal) {
+			maxVal = abs((float)(buffer[i]));
+		}
+	}
+	float multiplier = 32767 / maxVal;
+	for (int i = 0; i < size/2; i++) {
+		newBuffer[i] = (short)(floor((float)(buffer[i])) * multiplier);
+	}
+	return newBuffer;
+}
--- Normal.h
+++ Normal.h
@@ -1,11 +1,30 @@
-/*
-*/
-
+/** @file */
 #ifndef GROUP_PROJECT_NORMAL_H
 #define GROUP_PROJECT_NORMAL_H
 
-class Normal : public Iprocessable{
+#include "Iprocessable.h"
 
+/**
+ * A class for applying a normalization
+ */
+class Normal : public Iprocessable{
+public:
+
+	/**
+	 * Method that can be inherited to Echo, Normalization, and NoiseGate. Allows each class to process the given Wav File. Overload for 8 bit files. 
+	 * @param buffer - an array of unsigned chars constituting the audio data
+	 * @param size - an integer size of the buffer
+	 * @return - a processed array of unsigned chars
+	 */
+	unsigned char* processBuffer(unsigned char* buffer, int size) override;
+
+	/**
+	 * Method that can be inherited to Echo, Normalization, and NoiseGate. Allows each class to process the given Wav File. Overload for 8 bit files. 
+	 * @param buffer - an array of unsigned chars constituting the audio data
+	 * @param size - an integer size of the buffer
+	 * @return - a processed array of unsigned chars
+	 */
+	short* processBuffer(short* buffer, int size) override;
 };
 
 #endif //GROUP_PROJECT_NORMAL_H
